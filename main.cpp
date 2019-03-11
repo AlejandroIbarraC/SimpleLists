db7657355a20d3181eb5fb3842c7aee360cd0b00
FabianRamirez03 <ramirezfabian003@gmail.com>
2019-03-10 23:49:39 -0600

    Done all tests

--- main.cpp
+++ main.cpp
@@ -10,11 +10,11 @@ TEST(Listtests, ListStartsEmpty)
     List list;
     EXPECT_EQ(0, list.getLength());
 }
-int main(int argc, char* argv[]) {
 
-    Collector& thanos = Collector::getInstance();
+TEST(ListTests, createList)
+{
     List list;
-
+    Collector& thanos = Collector::getInstance();
     // Add items to list
     list.add(1);
     list.add(2);
@@ -22,45 +22,46 @@ int main(int argc, char* argv[]) {
     list.add(15);
     list.add(20);
     list.add(123);
-
+    EXPECT_EQ(6, list.getLength());
     // Print original list
     cout << "Original list:" << endl;
     list.display();
-
     // Print original collector
     cout << "Empty collector:" << endl;
     thanos.display();
-
     // Remove items
     cout << "Removing items..." << endl;
     list.remove(1);
     list.remove(2);
     list.remove(20);
-
+    EXPECT_EQ(3, list.getLength());
     // Print list with removals
     cout << "List with removals:" << endl;
     list.display();
-
     // Print collector with removals
     cout << "Collector with removals:" << endl;
     thanos.display();
-
-
     // Add new items
     cout << "Adding new items..." << endl;
     list.add(12);
     list.add(100);
     list.add(30);
-
+    EXPECT_EQ(6, list.getLength());
     // Print list with additions
     cout << "List with additions:" << endl;
     list.display();
-
     // Print collector with additions
     cout << "Collector with additions:" << endl;
     thanos.display();
+}
 
-    std::cout << "Goodbye, World!" << std::endl;
+
+
+
+
+int main(int argc, char* argv[]) {
+
+
 
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
